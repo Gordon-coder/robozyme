f63f08fa61a9ed166f893aaae7777ee3373d09e9
Gordon <gordonpeng27@gmail.com>
2024-10-07 11:36:41 +0800

    init webpages

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from './assets/logo.png'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav id="navbar" className="navbar bg-body-tertiary px-3" style={{height:80}}>
+        <img src={logo} height="40"/>
+        <ul className="nav nav-underline">
+          <li className="nav-item">
+            <a className="nav-link" href="#about">About Us</a>
+          </li>
+          <div className="vr"></div>
+          <li className="nav-item">
+            <a className="nav-link" href="#tech">Our Technology</a>
+          </li>
+          <div className="vr"></div>
+          <li className="nav-item">
+            <a className="nav-link" href="#product">Our Products</a>
+          </li>
+          <div className="vr"></div>
+          <li className="nav-item">
+            <a className="nav-link" href="#contact">Contact Us</a>
+          </li>
+        </ul>
+      </nav>
+        <div data-bs-spy="scroll" data-bs-target="#navbar" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" className="p-3" tabIndex={0}>
+          <div id="about">
+            <h2>About Us</h2><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          </div>
+          <div id="tech">
+            <h2>Our Technology</h2><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          </div>
+          <div id="product">
+            <h2>Our Products</h2><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          </div>
+          <div id="contact">
+            <h2>Contact Us</h2><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          </div>
+          .
+        </div>
     </>
   )
 }
