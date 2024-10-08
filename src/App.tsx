ab389e8e2e83ba14dd74c95b8fd226a76c9428d5
Gordon <gordonpeng27@gmail.com>
2024-10-08 13:41:39 +0800

    Add carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
 import logo from "./assets/logo.png";
+import placeholder_1 from "./assets/placeholder_1.png";
+import placeholder_2 from "./assets/placeholder_2.png";
+import placeholder_3 from "./assets/placeholder_3.png";
 import "bootstrap/dist/css/bootstrap.css";
 import ScrollSpy from "react-scrollspy-navigation";
+import Carousel from 'react-bootstrap/Carousel';
 
 function App() {
   return (
@@ -11,10 +14,10 @@ function App() {
         className="navbar bg-body-tertiary px-3 fixed-top"
         style={{ height: 80 }}
       >
-        <a className="nav-link" href="#">
+        <ScrollSpy activeClass="active" offsetTop={80}>
+        <a className="nav-link" href="#top">
           <img src={logo} height="40" />
         </a>
-        <ScrollSpy activeClass="active" offsetTop={80}>
           <ul className="nav nav-underline">
             <li className="nav-item">
               <a className="nav-link" href="#about">
@@ -43,6 +46,33 @@ function App() {
         </ScrollSpy>
       </nav>
       <div style={{ marginTop: 80 }}>
+        <div id="top">
+        <Carousel>
+      <Carousel.Item>
+        <img src={placeholder_1} className="w-100"/>
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={placeholder_3} className="w-100"/>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={placeholder_3} className="w-100"/>
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+        </div>
         <div id="about" className="p-3">
           <h2>About Us</h2>
         </div>
