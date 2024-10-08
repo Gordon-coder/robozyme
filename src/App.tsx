8111071f2b3b106277c42613aab0af0b549f97f3
Gordon <gordonpeng27@gmail.com>
2024-10-08 12:13:44 +0800

    add srollspy

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,63 @@
-import { useState } from 'react'
-import logo from './assets/logo.png'
-import 'bootstrap/dist/css/bootstrap.css'
+import { useState } from "react";
+import logo from "./assets/logo.png";
+import "bootstrap/dist/css/bootstrap.css";
+import ScrollSpy from "react-scrollspy-navigation";
 
 function App() {
   return (
     <>
-      <nav id="navbar" className="navbar bg-body-tertiary px-3" style={{height:80}}>
-        <img src={logo} height="40"/>
-        <ul className="nav nav-underline">
-          <li className="nav-item">
-            <a className="nav-link" href="#about">About Us</a>
-          </li>
-          <div className="vr"></div>
-          <li className="nav-item">
-            <a className="nav-link" href="#tech">Our Technology</a>
-          </li>
-          <div className="vr"></div>
-          <li className="nav-item">
-            <a className="nav-link" href="#product">Our Products</a>
-          </li>
-          <div className="vr"></div>
-          <li className="nav-item">
-            <a className="nav-link" href="#contact">Contact Us</a>
-          </li>
-        </ul>
+      <nav
+        id="navbar"
+        className="navbar bg-body-tertiary px-3 fixed-top"
+        style={{ height: 80 }}
+      >
+        <a className="nav-link" href="#">
+          <img src={logo} height="40" />
+        </a>
+        <ScrollSpy activeClass="active" offsetTop={80}>
+          <ul className="nav nav-underline">
+            <li className="nav-item">
+              <a className="nav-link" href="#about">
+                About Us
+              </a>
+            </li>
+            <div className="vr"></div>
+            <li className="nav-item">
+              <a className="nav-link" href="#tech">
+                Our Technology
+              </a>
+            </li>
+            <div className="vr"></div>
+            <li className="nav-item">
+              <a className="nav-link" href="#product">
+                Our Products
+              </a>
+            </li>
+            <div className="vr"></div>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </ScrollSpy>
       </nav>
-        <div data-bs-spy="scroll" data-bs-target="#navbar" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" className="p-3" tabIndex={0}>
-          <div id="about">
-            <h2>About Us</h2><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-          </div>
-          <div id="tech">
-            <h2>Our Technology</h2><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-          </div>
-          <div id="product">
-            <h2>Our Products</h2><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-          </div>
-          <div id="contact">
-            <h2>Contact Us</h2><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-          </div>
-          .
+      <div style={{ marginTop: 80 }}>
+        <div id="about" className="p-3">
+          <h2>About Us</h2>
         </div>
+        <div id="tech" className="p-3">
+          <h2>Our Technology</h2>
+        </div>
+        <div id="product" className="p-3">
+          <h2>Our Products</h2>
+        </div>
+        <div id="contact" className="p-3">
+          <h2>Contact Us</h2>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
