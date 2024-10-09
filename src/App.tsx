846f4cf2bc5c6a756816d599c6f471db1e8b675a
Gordon <gordonpeng27@gmail.com>
2024-10-09 11:45:06 +0800

    Make font size responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "./css/style.css"
 import placeholder_1 from "./assets/placeholder_1.png";
 import placeholder_2 from "./assets/placeholder_2.png";
 import placeholder_3 from "./assets/placeholder_3.png";
@@ -12,16 +14,14 @@ function App() {
   return (
     <>
       <Navagation />
-      <div id="top">
-        <div style={{ paddingTop: 100 }}></div>
-        <Carousel>
-          {[placeholder_1, placeholder_2, placeholder_3].map((image) => (
-            <Carousel.Item key={image}>
-              <img src={image} className="w-100" />
-            </Carousel.Item>
-          ))}
-        </Carousel>
-      </div>
+      <div className="h-100px"></div>
+      <Carousel>
+        {[placeholder_1, placeholder_2, placeholder_3].map((image) => (
+          <Carousel.Item key={image}>
+            <img src={image} className="w-100" />
+          </Carousel.Item>
+        ))}
+      </Carousel>
       <About />
       <Tech />
       <Product />
