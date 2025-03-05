c3497a52ee9daa016a6b2c5f6b817218224d9d4f
Gordon <gordonpeng27@gmail.com>
2025-03-05 20:51:24 +0800

    Add contact email

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,9 @@ function Contact() {
         <div id="contact" className="p-3">
         <div style={{ paddingTop: 20 }}></div>
         <h1 className="text-center">Contact Us</h1>
-
+        <div className="text-center">
+          Email: <a href="https://mail.google.com/mail/?view=cm&fs=1&to=info@robozyme-limited.com" target="_blank">info@robozyme-limited.com</a>
+        </div>
       </div>
         </>
     )
