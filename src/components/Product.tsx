0bff03b76a69e3cf52d99151066ffad3586eac8d
gordon-coder <gordonpeng27@gmail.com>
2026-02-03 20:11:27 +0800

    Changed product cards to images

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,27 +1,33 @@
-import ProductCard from "./ProductCard";
 import { useState } from "react";
+import Carousel from 'react-bootstrap/Carousel';
+import OurProductRobozyme1 from "../assets/OurProductRobozyme1.png";
+import OurProductRobozyme2 from "../assets/OurProductRobozyme2.png";
+import OurProductRobozyme3 from "../assets/OurProductRobozyme3.png";
+
 
 function Product() {
-  let products = [
-    { title: "Putrescine (1,4-diaminebutane)", body: "Polyamide precursors" },
-    { title: "L-tert-leucine", body: "Chiral precursor for tumor-fighting agents/anti-HIV protease inhibitors" },
-    { title: "Ethyl (R)-3-hydoxybutanoate (R-EHB): ", body: "Chiral intermediate for pharmaceutic active ingredients, such as L-carnitine, (R)-4-amino-3-hydroxybutyric acid, GABOB" },
-  ];
-
   let [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex: number) => {
+    setIndex(selectedIndex);
+  }
+
   return (
     <>
       <div id="product" className="p-3">
-        <div style={{ paddingTop: 20 }}></div>
         <h1 className="text-center">Our Products</h1>
-        <div className="d-flex justify-content-center">
-        <button onClick={()=>(setIndex((index==0)?products.length-1:index-1))} className="width-1">{"<"}</button>
-          <ProductCard
-            title={products[index].title}
-            body={products[index].body}
-          />
-        <button onClick={()=>(setIndex((index==products.length-1)?0:index+1))} className="width-1">{">"}</button>
-      </div></div>
+        <Carousel className="width-12" activeIndex={index} onSelect={handleSelect} data-bs-theme="dark" style={{margin:"auto"}}>
+          <Carousel.Item>
+            <img src={OurProductRobozyme1} className="d-block w-100" alt="First slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={OurProductRobozyme2} className="d-block w-100" alt="Second slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={OurProductRobozyme3} className="d-block w-100" alt="Third slide" />
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </>
   );
 }
